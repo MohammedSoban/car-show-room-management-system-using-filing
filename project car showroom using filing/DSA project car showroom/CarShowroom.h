63b5da1963e460068ea605728c76c4b4dd2af653
Muhmmad Soban <30773814+MohammedSoban@users.noreply.github.com>
2018-11-06 20:24:05 +0500

    Add files via upload

---
+++ project car showroom using filing/DSA project car showroom/CarShowroom.h
@@ -0,0 +1,36 @@
+#include<iostream>
+#include"string"
+#include<Windows.h>
+
+using namespace std;
+
+
+
+class CarShowroom
+{
+
+private:
+
+	
+	string _RegistrationNumber;
+	string _NameOfCar;
+	string _Make;
+	string _Model;
+	string _CubicCentimeter;
+	string _Colour;
+	string _Type;
+	string _NameOfowner; 
+	string _price;
+	string _NewOrOld;
+	
+
+public:
+	
+
+	void AddCar();
+	void ModifyCar();
+	void DelCar();
+	void DisplayCars();
+	void SearchCar();
+
+};
